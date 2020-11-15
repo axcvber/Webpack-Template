8a10bdfd2e2533afc5817031b8fd86616105b4e9
Alexander <axlisovsky@gmail.com>
2020-11-15 13:21:55 +0200

    webpack

---
+++ src/assets/js/main.js
@@ -0,0 +1,3 @@
+const calc = (a, b) => a + b
+
+console.log('This fucking work', calc(2, 3))
---
+++ src/index.js
@@ -0,0 +1,11 @@
+// JS
+import './assets/js/main'
+
+// SCSS
+import './assets/scss/style.scss'
+
+// //Less 
+// import './assets/less/style.less'
+
+// CSS (example)
+// import './assets/css/main.css'
